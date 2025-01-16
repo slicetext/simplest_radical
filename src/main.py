2eeeac3a3bea2534b36e28830f9df30c0078123b
slicetext <theokozak@gmail.com>
2025-01-15 20:16:55 -0700

    fixed source tree

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import math
 squares=[]
+tree={0:[""]}
 def gen_squares(n):
     for i in range(2,n+1):
         squares.append(i*i)
@@ -17,17 +18,49 @@ def isInteger(n):
     if n%2 == 0 or (n+1)%2 == 0:
         return True
     return False
-def square_root(num):
+def square_root(num,index=0,index2=1):
     global remainder
+    if(index not in tree):
+        tree[index]=[""]
+    if(index+1 not in tree):
+        tree[index+1]=[""]
+    while(len(tree[index])<index2+1):
+        tree[index].append("")
+    while(len(tree[index+1])<index2+1):
+        tree[index+1].append("")
+    tree[index][index2]=int(num)
     if(isInteger(math.sqrt(num))):
+        tree[index+1][index2]=str(int(math.sqrt(num)))+"*"
         return math.sqrt(num)
     for i in squares:
         if(num/i>1 and isInteger(num/i)):
-            return square_root(i)*square_root(num/i)
+            return square_root(int(i),index+1,index2-1)*square_root(int(num/i),index+1,index2+1)
     remainder=remainder+num
     return 1
 
 while True:
     num=float(input("Get root of number:"))
     print(str(square_root(num))+" √"+str(remainder))
+    longest=0
+    num_cols=0
+    cols_row={}
+    for i in range(len(tree)):
+        if(len(tree[i])>num_cols):
+            num_cols=len(tree[i])
+        col_count=0
+        cols_row[i]=0
+        for j in tree[i]:
+            if(len(str(j))>longest):
+                longest=len(str(j))
+            cols_row[i]=cols_row[i]+len(str(j))
+    #print(" "*int(longest*num_cols/2),end="")
+    for i in range(len(tree)):
+        cols=1
+        chars=cols_row[i]
+        #print(" "*(longest*num_cols-chars-1),end="")
+        for j in tree[i]:
+            print(j,end=" "*(longest-len(str(j))))
+            cols=cols+1
+        print("")
     remainder=0
+    tree={0:[]}
